b3a71b1eca3342ad61816d9e2957fa808f5a49f8
EUNA KWON <108185369+827euni@users.noreply.github.com>
2024-01-24 23:00:58 +0900

    Title: 최댓값 구하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240124/최댓값 구하기/maximum-value.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] arr = s.split(" ");
+
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[1]);
+        int c = Integer.parseInt(arr[2]);
+
+        if (a>b) {
+            if (b>c){
+                System.out.println(a);
+            }
+            
+            else {
+                if (a>c) {
+                    System.out.println(a);
+                }
+                else{
+                    System.out.println(c);
+                }
+            }
+        }
+
+        else {
+            if (c>a){
+                if (c>b){
+                    System.out.println(c);
+                }
+                else{
+                    System.out.println(b);
+                }
+            }
+
+            else{
+                System.out.println(b);
+            }
+        }
+    }}
