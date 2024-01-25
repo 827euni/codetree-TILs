6ae1a406ffdd6f541d224cd7116507e7dbde8703
EUNA KWON <108185369+827euni@users.noreply.github.com>
2024-01-25 13:58:11 +0900

    Title: 별표 출력하기 5, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

--- 240125/별표 출력하기 5/print-star-5.java
+++ 240125/별표 출력하기 5/print-star-5.java
@@ -4,13 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        String[] arr = s.split(" ");
+        String s = sc.next();
         int n = Integer.parseInt(s);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < (n - i); j++) {
-                for (int k = 0; k < (n-i); k++){
+        for (int i = 0; i < n; i++) { //세로축 
+            for (int j = 0; j < (n - i); j++) { //가로축
+                for (int k = 0; k < (n-i); k++){ //별 찍기 반복
                     System.out.print("*");
                 }
                 System.out.print(" ");
