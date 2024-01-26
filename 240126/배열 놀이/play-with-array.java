51013f33ed24a8a9a5c64ee6738416ac0a295adb
EUNA KWON <108185369+827euni@users.noreply.github.com>
2024-01-26 21:53:56 +0900

    Title: 배열 놀이, Time: 160ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240126/배열 놀이/play-with-array.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int[] arrN = new int[n];
+
+        for (int i = 0; i < n; i++){
+            arrN[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < q; i++){
+            int type = sc.nextInt();
+            int a = sc.nextInt();
+            int b;
+            int idx = -1;
+
+            if (type == 1){
+                System.out.println(arrN[a-1]);
+            }
+
+            else if (type == 2){
+                for (int j = 0; j<n; j++){
+                    if (arrN[j] == a){
+                        idx = j + 1;
+                        break;
+                    }
+                }
+                System.out.println(idx);
+            }
+
+            else {
+                b = sc.nextInt();
+                for (int k = a-1; k<b; k++){
+                    System.out.print(arrN[k] + " ");
+                }
+
+            }
+    }
+}
+}
