36cefd9fd68cb11046edc944754bbbbd4f876837
EUNA KWON <108185369+827euni@users.noreply.github.com>
2024-01-28 22:51:21 +0900

    Title: 두 배열의 곱, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240128/두 배열의 곱/multiple-of-two-arrays.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr1 = new int[3][3];
+        int[][] arr2 = new int[3][3];
+        int[][] result = new  int[3][3];
+        
+        for (int i = 0; i<3; i++) {
+            for (int j = 0; j<3; j++) {
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i<3; i++) {
+            for (int j = 0; j<3; j++) {
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i<3; i++) {
+            for (int j = 0; j<3; j++) {
+                result[i][j] = arr1[i][j] * arr2[i][j];
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
