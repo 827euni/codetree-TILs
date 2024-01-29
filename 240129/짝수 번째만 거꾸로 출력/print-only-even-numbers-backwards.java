bd2ab95b2e1422cdf59fbf3a217e6aeb588096c1
EUNA KWON <108185369+827euni@users.noreply.github.com>
2024-01-29 21:44:07 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240129/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
+++ 240129/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
@@ -4,8 +4,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String s = sc.next();
-        for (int i = s.length()-1; i>=0; i-=2){
-            System.out.print(s.charAt(i));
+        if (s.length() % 2 == 0){
+            for (int i = s.length()-1; i>=0; i-=2){
+                System.out.print(s.charAt(i));
+            }
+        }
+        else { 
+            for (int i = s.length()-2; i>=0; i-=2){
+                System.out.print(s.charAt(i));
+            }
         }
     }
 }
