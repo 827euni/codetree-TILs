6b0b75b954e5234372f2b57f76f49b1911f7bbfa
EUNA KWON <108185369+827euni@users.noreply.github.com>
2024-01-29 21:11:26 +0900

    Title: 가장 짧은 문자열, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240129/가장 짧은 문자열/shortest-string.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        String c = sc.next();
+
+        int maxNum = Math.max(a.length(), Math.max(b.length(), c.length()));
+        int minNum = Math.min(a.length(), Math.max(b.length(), c.length()));
+
+        System.out.println(maxNum-minNum);
+    }
+}
