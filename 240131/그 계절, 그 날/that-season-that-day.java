4beddd69dca603fbaea6bf3ef26e7b6f0a6731ee
EUNA KWON <108185369+827euni@users.noreply.github.com>
2024-01-31 21:07:17 +0900

    Title: 그 계절, 그 날, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240131/그 계절, 그 날/that-season-that-day.java
+++ 240131/그 계절, 그 날/that-season-that-day.java
@@ -24,8 +24,47 @@ public class Main {
     }
 
     public static boolean isExist(int y, int m, int d){
+        boolean feb = isFeb(y);
+        
+        if (y>3000){
+            return false;
+        }
+        
+        if (m>12 || m<=0){
+            return false;
+        }
+
+        if (((m == 4 || m == 6 || m == 9 || m == 11) && (d < 1 || d > 30)) || ((m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) && (d < 1 || d > 31))) {
+            return false;
+        }
+
+
+        if (feb==true && d > 29){
+            return false;
+        }
+
+        if (feb = false && d > 28){
+            return false;
+        }
+
         return true;
     }
+
+    public static boolean isFeb(int y){
+        if (y%4==0){
+            return true;
+        }
+
+        if (y%4==0 && y%100==0){
+            return false;
+        }
+        if (y%4 ==0 && y%100 ==0 && y%400 == 0){
+            return true;
+        }
+
+        return false;
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
