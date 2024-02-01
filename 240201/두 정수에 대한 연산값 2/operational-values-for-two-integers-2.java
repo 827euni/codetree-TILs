b8bd8c16869f693cf3ebba90a48f685e10329ace
EUNA KWON <108185369+827euni@users.noreply.github.com>
2024-02-01 22:33:12 +0900

    Title: 두 정수에 대한 연산값 2, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240201/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
+++ 240201/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -7,8 +7,12 @@ public class Main {
 
         min += 10;
         max *= 2;
-
+        if (Math.min(a,b) == a){
         System.out.print(min + " " + max);
+        }
+        else {
+             System.out.print(max + " " + min);
+        }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
