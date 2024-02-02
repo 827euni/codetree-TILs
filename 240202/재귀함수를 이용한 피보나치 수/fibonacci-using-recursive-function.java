173c0304aa5f120412d9b6bec7838c0621bcc92a
EUNA KWON <108185369+827euni@users.noreply.github.com>
2024-02-02 21:46:32 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240202/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int Fibo(int n){
+
+        if (n==1){
+            return 1;
+        }
+
+        if (n==2){
+            return 1;
+        }
+
+        return Fibo(n-2) + Fibo(n-1);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(Fibo(n));
+
+    }
+}
