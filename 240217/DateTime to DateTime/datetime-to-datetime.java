09139938651bb6d9128c5cbed70e7adb4bed55bf
EUNA KWON <108185369+827euni@users.noreply.github.com>
2024-02-17 20:34:56 +0900

    Title: DateTime to DateTime, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240217/DateTime to DateTime/datetime-to-datetime.java
+++ 240217/DateTime to DateTime/datetime-to-datetime.java
@@ -9,6 +9,11 @@ public class Main {
         int result;
 
         result = (a-11) * 24 * 60 + (b-11) * 60 + (c-11);
-        System.out.println(result);
+        if (result>0){ 
+            System.out.println(result);
+        }
+        else{
+            System.out.println(-1);
+        }
     }
 }
