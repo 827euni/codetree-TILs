4452bda36cf43d8d5a607c1adf41fcb74a110c5f
EUNA KWON <108185369+827euni@users.noreply.github.com>
2024-03-21 22:00:04 +0900

    Title: 최대로 겹치는 지점, Time: 195ms, Memory: 10MB, Status: Passed - CodeTree

--- 240321/최대로 겹치는 지점/maximum-overlapped-points.java
+++ 240321/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -14,13 +14,13 @@ public class Main {
         for (int i = 0; i<n; i++){
             int x1 = sc.nextInt();
             int x2 = sc.nextInt();
-            for (int j = x1; j<x2; j++){
+            for (int j = x1; j<=x2; j++){
                 arr[j]+=1;
             }
         }
         for (int i = 0; i <= 200; i++) {
             count = Math.max(count, arr[i]);
         }
-        System.out.println(count+1);
+        System.out.println(count);
     }
 }
